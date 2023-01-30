5bab22621c0946465a711399d470e8d1a3b6e5d6
Mohsen Alkadri <mehsen2222@gmail.com>
2023-01-30 15:16:19 +0200

    colored the log

--- lib/bloc_observer.dart
+++ lib/bloc_observer.dart
@@ -5,30 +5,36 @@ class MyBlocObserver extends BlocObserver {
   @override
   void onCreate(BlocBase bloc) {
     super.onCreate(bloc);
-    debugPrint('onCreate -- ${bloc.runtimeType}');
+    debugPrint('\x1B[30m onCreate -- ${bloc.runtimeType}');
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    debugPrint('\x1B[35m onEvent -- ${bloc.runtimeType}, $event');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    debugPrint('onTransition -- ${bloc.runtimeType}, $transition');
+    debugPrint('\x1B[36m onTransition -- ${bloc.runtimeType}, $transition');
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    debugPrint('onChange -- ${bloc.runtimeType}, $change');
+    debugPrint('\x1B[32m onChange -- ${bloc.runtimeType}, $change');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    debugPrint('onError -- ${bloc.runtimeType}, $error');
+    debugPrint('\x1B[31m onError -- ${bloc.runtimeType}, $error');
     super.onError(bloc, error, stackTrace);
   }
 
   @override
   void onClose(BlocBase bloc) {
     super.onClose(bloc);
-    debugPrint('onClose -- ${bloc.runtimeType}');
+    debugPrint('\x1B[30m onClose -- ${bloc.runtimeType}');
   }
 }
