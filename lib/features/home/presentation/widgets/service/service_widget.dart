6d1ad2a4294b53bc0cebe2edc3b469445011f6d1
Mohammad Assaf10 <mhamad234assaf@gmail.com>
2023-03-23 10:19:07 +0200

    finish app

--- lib/features/home/presentation/widgets/service/service_widget.dart
+++ lib/features/home/presentation/widgets/service/service_widget.dart
@@ -1,5 +1,6 @@
 import 'package:asrar_app/config/app_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../../config/color_manager.dart';
@@ -7,6 +8,8 @@ import '../../../../../config/routes_manager.dart';
 import '../../../../../config/strings_manager.dart';
 import '../../../../../config/styles_manager.dart';
 import '../../../../../config/values_manager.dart';
+import '../../../../../core/app/functions.dart';
+import '../../../../auth/presentation/bloc/authentication_bloc.dart';
 import '../../../domain/entities/service_entities.dart';
 
 class ServiceWidget extends StatelessWidget {
@@ -14,16 +17,23 @@ class ServiceWidget extends StatelessWidget {
     super.key,
     required this.service,
   });
+
   final ServiceEntities service;
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        Navigator.pushNamed(
-          context,
-          Routes.instructionsRoute,
-          arguments: service,
-        );
+        final authState = BlocProvider.of<AuthenticationBloc>(context).state;
+        if (authState.status == AuthStatus.loggedIn) {
+          Navigator.pushNamed(
+            context,
+            Routes.instructionsRoute,
+            arguments: service,
+          );
+        } else {
+          showLoginDialog(context);
+        }
       },
       child: Container(
         margin: EdgeInsets.symmetric(
@@ -58,7 +68,9 @@ class ServiceWidget extends StatelessWidget {
                       radius: AppSize.s34.r,
                       backgroundColor: ColorManager.darkWhite,
                       child: Text(
-                        AppStrings.asrarServices.tr(context).replaceAll(" ", "\n"),
+                        AppStrings.asrarServices
+                            .tr(context)
+                            .replaceAll(" ", "\n"),
                         textAlign: TextAlign.center,
                         style: getAlmaraiBoldStyle(
                           fontSize: AppSize.s16.sp,
